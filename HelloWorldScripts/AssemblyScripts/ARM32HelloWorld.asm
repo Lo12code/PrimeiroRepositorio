e7c11ca654fd3fda23e02a211b1d84926aae07ea
Lorenzo <lorenzodr95@hotmail.com>
2021-06-20 13:31:48 -0300

    Adicionando Hello World de ARM32

---
+++ HelloWorldScripts/AssemblyScripts/ARM32HelloWorld.asm
@@ -0,0 +1,17 @@
+.global _start
+.section .text
+
+_start:
+	mov r7, #0x4
+	mov r0, #2
+	ldr r1 =message
+	mov r2, #13
+	swi 0
+
+	mov r7, #0x01
+	mov r0, #0
+	swi 0
+
+.section .data
+	message:
+	.ascii "Hello World!\n"
